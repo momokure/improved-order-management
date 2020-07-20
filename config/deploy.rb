28d2f559bfff1f1be7c39745390fef2a6d07b6b8
hanaharafumiki <hanahara@cornerprinting.com>
2020-07-21 00:08:29 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, "improved-order-management"
 set :repo_url, "git@github.com:hanaharafumiki/improved-order-management.git"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :rbenv_type, :user
-set :rbenv_ruby, '2.5.1'
+set :rbenv_ruby, '2.6.5'
 set :ssh_options, auth_methods: ['publickey'], keys: ['~/.ssh/hanaharafumiki.pem'] 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
