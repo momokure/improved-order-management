d65c96faf6f836ab6143e59348ff0687609dc8f5
hanaharafumiki <hanahara@cornerprinting.com>
2020-07-27 20:18:48 +0900

    Update 02_departments.rb

--- db/fixtures/02_departments.rb
+++ db/fixtures/02_departments.rb
@@ -1,6 +1,20 @@
 Department.seed(
-  {id: 1, department_name: "営業"},
-  {id: 2, department_name: "経理"},
-  {id: 3, department_name: "データ・指示書"},
-  {id: 4, department_name: "発注"}
+  {id: 1, department_name: "システム管理"},
+  {id: 2, department_name: "ご注文管理"},
+  {id: 3, department_name: "営業"},
+  {id: 4, department_name: "経理"},
+  {id: 5, department_name: "データ/指示書"},
+  {id: 6, department_name: "国内発注"},
+  {id: 7, department_name: "国内仕分け"},
+  {id: 8, department_name: "海外発注"},
+  {id: 9, department_name: "海外仕分け"},
+  {id: 10, department_name: "シルクスクリーンA"},
+  {id: 11, department_name: "シルクスクリーンB"},
+  {id: 12, department_name: "シルクスクリーンC"},
+  {id: 13, department_name: "インクジェット"},
+  {id: 14, department_name: "刺繍"},
+  {id: 15, department_name: "UV"},
+  {id: 16, department_name: "縫製"},
+  {id: 17, department_name: "シルクスクリーンD"},
+  {id: 18, department_name: "発送"}
 )
