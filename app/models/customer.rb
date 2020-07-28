e2a5b676ea89abe7f44bc249ecd423eaf93726aa
hanaharafumiki <hanahara@cornerprinting.com>
2020-07-28 20:25:32 +0900

    Update customer.rb

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,9 @@
 class Customer < ApplicationRecord
   belongs_to :customer_type
   has_many :customer_notes
-  accepts_nested_attributes_for :customer_notes
+  accepts_nested_attributes_for :customer_notes, reject_if: :chk_reject
+
+  def chk_reject(attributed)
+    attributed['customer_note'].blank?
+  end
 end
