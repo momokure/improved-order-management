21c969b7e853e146e15dc8e6bb7eb977b321f947
hanaharafumiki <hanahara@cornerprinting.com>
2020-07-29 15:54:29 +0900

    領収書テーブルの削除

--- app/models/receipt_required.rb
+++
@@ -1,4 +0,0 @@
-class ReceiptRequired < ApplicationRecord
-  has_many :customers
-  has_many :companies
-end
--- db/migrate/20200728061529_create_receipt_requireds.rb
+++
@@ -1,8 +0,0 @@
-class CreateReceiptRequireds < ActiveRecord::Migration[5.2]
-  def change
-    create_table :receipt_requireds do |t|
-      t.string :receipt_required
-      t.timestamps
-    end
-  end
-end
