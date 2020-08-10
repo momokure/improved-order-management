de26e1652a60e4c928dfb5048eed63d9456d1214
hanaharafumiki <hanahara@cornerprinting.com>
2020-08-10 14:34:41 +0900

    registerアクションの削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :customers
-  resources :orders do
-    collection do
-      get 'register'
-    end
-  end
+  resources :orders
 end
