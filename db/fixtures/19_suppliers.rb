93052ec43969954e6bff536aae6602b9e92860df
hanaharafumiki <hanahara@cornerprinting.com>
2020-08-16 01:51:58 +0900

    fixturesのアップデート

--- db/fixtures/19_suppliers.rb
+++ db/fixtures/19_suppliers.rb
@@ -1,6 +1,15 @@
 Supplier.seed(
-  {id: 1, supplier_name: "T-sele"},
-  {id: 2, supplier_name: "🚢海外発注"},
-  {id: 3, supplier_name: "🏭社内在庫"},
-  {id: 4, supplier_name: "🍡お持込(発注不要)"}
+  {id: 1, supplier_name: "三浦商事"},
+  {id: 2, supplier_name: "康貿易(T-Selection)"},
+  {id: 3, supplier_name: "VIA TRANSPORTS(ANYTEES)"},
+  {id: 4, supplier_name: "GILDAN BRANDS"},
+  {id: 5, supplier_name: "CAB"},
+  {id: 6, supplier_name: "TOMS"},
+  {id: 7, supplier_name: "FELIC"},
+  {id: 8, supplier_name: "松栄シルク"},
+  {id: 9, supplier_name: "ボンマックス"},
+  {id: 10, supplier_name: "トレードワークス(MARKLESS STYLE)"},
+  {id: 11, supplier_name: "その他 - 国内"},
+  {id: 12, supplier_name: "その他 - 海外"},
+  {id: 13, supplier_name: "🍡お持込(発注不要)"}
 )
