d6da96944cc400c84a8421a637d3cbd1b9b0a572
hanaharafumiki <hanahara@cornerprinting.com>
2020-08-15 17:36:12 +0900

    経理一覧の修正

---
+++ app/controllers/accounting/wip_controller.rb
@@ -0,0 +1,8 @@
+class Accounting::WipController < ApplicationController
+  require "date"
+  def index
+    # @day = Date.today
+    # @orders = Order.left_joins(:payments).where(payments: { order_id: nil })
+    # @payment = Payment.new
+  end
+end
