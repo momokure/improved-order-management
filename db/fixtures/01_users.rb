852598463d750ad045899e96a77994dab783b644
hanaharafumiki <hanahara@cornerprinting.com>
2020-08-18 23:03:05 +0900

    Update 01_users.rb

--- db/fixtures/01_users.rb
+++ db/fixtures/01_users.rb
@@ -8,7 +8,7 @@ end
 
 User.seed do |s|
   s.id = 2
-  s.user_name = "花原史樹"
+  s.user_name = "花原 史樹"
   s.email = "hanahara@cornerprinting.com"
   s.password = "cp201405"
   s.password_confirmation = "cp201405"
