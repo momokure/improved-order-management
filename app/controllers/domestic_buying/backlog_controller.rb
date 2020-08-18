338929e49fff652136fcb5a10e372ef918303963
hanaharafumiki <hanahara@cornerprinting.com>
2020-08-18 11:55:06 +0900

    経理フォームの修正

--- app/controllers/domestic_buying/backlog_controller.rb
+++ app/controllers/domestic_buying/backlog_controller.rb
@@ -1,10 +1,16 @@
 class DomesticBuying::BacklogController < ApplicationController
-  require "date"
   def index
-    @orders = Order.left_joins(:payments).where(payments: { order_id: nil })
-                .where.not(orders: { internal_delivery_date: nil })
-                .order(:internal_delivery_date)
-    # @day = Date.today
-    # @payment = Payment.new
+    # @orders = Order.left_joins(order_details: :buy_details)
+    #             .where(buy_details: { order_detail_id: nil })
+    #             .or(Order.left_joins(order_details: :buy_details)
+    #                   .where.not(buy_details: { buy_progress_id: 1 })
+    #             )
+
+    @orders = Order.left_joins(order_details: :buy_details)
+                      .where(buy_details: { buy_progress_id: [2, 3, 4] })
+
+    # @orders = Order.left_joins(:order_details).where(payments: { order_id: nil })
+    #             .where.not(orders: { internal_delivery_date: nil })
+    #             .order(:internal_delivery_date)
   end
 end
