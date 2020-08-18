2f96b05d76ad6279037f83c4c45f8dd36ec8a972
hanaharafumiki <hanahara@cornerprinting.com>
2020-08-18 10:06:39 +0900

    Update deadline_controller.rb

--- app/controllers/accounting/deadline_controller.rb
+++ app/controllers/accounting/deadline_controller.rb
@@ -2,6 +2,7 @@ class Accounting::DeadlineController < ApplicationController
   require "date"
   def index
     @orders = Order.left_joins(:payments).where(payments: { order_id: nil })
+                .where.not(orders: { payment_deadline_date: nil })
                 .order(:payment_deadline_date)
   end
 end
