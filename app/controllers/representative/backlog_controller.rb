059bde1b4fe33eb3de0634fc4e7640c997d52a42
hanaharafumiki <hanahara@cornerprinting.com>
2020-08-18 16:40:19 +0900

    対応件数の取得

--- app/controllers/representative/backlog_controller.rb
+++ app/controllers/representative/backlog_controller.rb
@@ -1,6 +1,8 @@
 class Representative::BacklogController < ApplicationController
   def index
     @orders = Order.where(representative_user_id: 1, first_response_date: nil)
+    @from = Date.today
+    @to = @from+1
   end
 
   def update
