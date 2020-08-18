71d7b3857225c297ad2166869ae12352149f3307
hanaharafumiki <hanahara@cornerprinting.com>
2020-08-18 09:36:12 +0900

    Update backlog_controller.rb

--- app/controllers/accounting/backlog_controller.rb
+++ app/controllers/accounting/backlog_controller.rb
@@ -1,7 +1,9 @@
 class Accounting::BacklogController < ApplicationController
   require "date"
   def index
-    @orders = Order.left_joins(:payments).where(payments: { order_id: nil }).where.not(orders: {internal_delivery_date: nil })
+    @orders = Order.left_joins(:payments).where(payments: { order_id: nil })
+                .where.not(orders: { internal_delivery_date: nil })
+                .order(orders: { :internal_delivery_date })
     @day = Date.today
     @payment = Payment.new
   end
