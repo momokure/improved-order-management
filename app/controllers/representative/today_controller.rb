8cb82b61f47e039d16716e4ad34d4afa9fc3633f
hanaharafumiki <hanahara@cornerprinting.com>
2020-08-19 10:01:09 +0900

    件数取得の設定

--- app/controllers/representative/today_controller.rb
+++ app/controllers/representative/today_controller.rb
@@ -1,7 +1,7 @@
 class Representative::TodayController < ApplicationController
   def index
     @orders = Order.all
-    @from = Date.today
+    @from = DateTime.now.beginning_of_day - 9.hour
     @to = @from+1
   end
 end
