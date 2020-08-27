55ade47827f63f29cc61f6d5c31fe2ca8ef52e7e
hanaharafumiki <hanahara@cornerprinting.com>
2020-08-27 09:37:51 +0900

    ビューの修正

--- app/controllers/domestic_buying/backlog_all_controller.rb
+++ app/controllers/domestic_buying/backlog_all_controller.rb
@@ -21,15 +21,3 @@ class DomesticBuying::BacklogAllController < ApplicationController
                 # .order(:internal_delivery_date)
   end
 end
-
-   # .where(buy_details: { buying_user_id: nil }).distinct
-   # .where.not(buy_details: { buying_user_id: [29, 32, 33] }).distinct
-
-# 国内発注ありで
-# 発注詳細が未制作で
-# 社内納期がある
-#
-# または
-# 国内発注ありで
-# 海外発注済 (=担当者が奥田さん) がありだけど
-   # 国内未発注 (=担当者が藤田さん、井上さん、小田さんがいない)
