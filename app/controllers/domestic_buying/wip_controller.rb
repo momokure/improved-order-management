586957c2d8e5e7c46a4337aff4d756baaa5035a5
hanaharafumiki <hanahara@cornerprinting.com>
2020-08-27 09:41:45 +0900

    Update wip_controller.rb

--- app/controllers/domestic_buying/wip_controller.rb
+++ app/controllers/domestic_buying/wip_controller.rb
@@ -2,12 +2,6 @@ class DomesticBuying::WipController < ApplicationController
   def index
     @orders = Order.left_joins(:buy_details)
                 .where.not(buy_details: { buy_progress_id: 2 }).distinct
-                .where.not(orders: { internal_delivery_date: nil }).distinct
-                .order(:internal_delivery_date)
-
-    @orders = Order.left_joins(:buy_details).distinct
-                .where(orders: { domestic_buying: 1 }).distinct
-                .where.not(buy_details: { buying_user: 54 }).distinct
                 .order(:internal_delivery_date)
   end
 end
