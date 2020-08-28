515189b6ed06cca754118c0b9f104cda0153d9d7
hanaharafumiki <hanahara@cornerprinting.com>
2020-08-28 09:45:45 +0900

    Update backlog_controller.rb

--- app/controllers/overseas_buying/backlog_controller.rb
+++ app/controllers/overseas_buying/backlog_controller.rb
@@ -9,7 +9,7 @@ class OverseasBuying::BacklogController < ApplicationController
                   Order.left_joins(:buy_details).distinct
                     .where(orders: { overseas_buying: 1 }).distinct
                     .where.not(buy_details: { order_id: nil }).distinct
-                    .where(buy_details: { buying_user_id: [29, 32, 33] }).distinct
+                    .where(buy_details: { buying_user_id: [54, 29, 32, 33] }).distinct
                     .order(:internal_delivery_date)
                 )
   end
