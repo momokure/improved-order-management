dfe86a784380e84d835483fa0dd12ed532426b68
hanaharafumiki <hanahara@cornerprinting.com>
2020-09-02 18:38:35 +0900

    海外発注シートの色分け

--- app/controllers/overseas_buying/all_controller.rb
+++ app/controllers/overseas_buying/all_controller.rb
@@ -5,12 +5,19 @@ class OverseasBuying::AllController < ApplicationController
                 .where(buy_details: { order_id: nil }).distinct
                 .where.not(orders: { internal_delivery_date: nil }).distinct
                 .order(:internal_delivery_date)
+                .order(:desired_delivery_type_id)
+                .order(:order_type_id)
                 .or(
                   Order.left_joins(:buy_details).distinct
                     .where(orders: { overseas_buying: 1 }).distinct
                     .where.not(buy_details: { order_id: nil }).distinct
                     .where(buy_details: { buying_user_id: [54, 29, 32, 33] }).distinct
                     .order(:internal_delivery_date)
+                    .order(:desired_delivery_type_id)
+                    .order(:order_type_id)
                 )
   end
 end
+
+# .where(buy_details: { buying_user_id: [54, 29, 32, 33] }).distinct
+# .where(buy_details: { buy_type_id: 2 }).distinct
