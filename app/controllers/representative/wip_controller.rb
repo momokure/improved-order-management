82c7c1605eaa259e42d402fff1d53a9bf40b197e
hanaharafumiki <hanahara@cornerprinting.com>
2020-09-06 22:14:56 +0900

    営業成績の実装(途中)

--- app/controllers/representative/wip_controller.rb
+++ app/controllers/representative/wip_controller.rb
@@ -3,5 +3,11 @@ class Representative::WipController < ApplicationController
 
   def index
     @orders = Order.where(representative_user_id: current_user.id, payment_confirmation: "unconfirmed")
+
+    @year = Date.today.year
+    @this_month = Date.today.month
+    @last_month = Date.today.month-1
+    @this_month = @year.to_s + "-0" + @this_month.to_s
+    @last_month = @year.to_s + "-0" + @last_month.to_s
   end
 end
