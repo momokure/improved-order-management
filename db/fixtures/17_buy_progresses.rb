ed329ee3c32e024df55f7c0ade623c35ddc0c0f3
hanaharafumiki <hanahara@cornerprinting.com>
2020-09-10 21:34:51 +0900

    ヘッダー表示名修正

--- db/fixtures/17_buy_progresses.rb
+++ db/fixtures/17_buy_progresses.rb
@@ -1,6 +1,6 @@
 BuyProgress.seed(
-  {id: 1, buy_progress_name: "発注作業中"},
-  {id: 2, buy_progress_name: "✔️️発注完了"},
+  {id: 1, buy_progress_name: "⏳作業中"},
+  {id: 2, buy_progress_name: "✔️️完了"},
   {id: 3, buy_progress_name: "⚠️欠品あり"},
   {id: 4, buy_progress_name: "🔄変更あり"},
   {id: 5, buy_progress_name: "💴🍐ご入金なし"}
