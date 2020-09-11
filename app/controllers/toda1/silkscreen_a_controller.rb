6a308fafea0a27d79927188c1a7248c85ed05de5
hanaharafumiki <hanahara@cornerprinting.com>
2020-09-11 10:20:45 +0900

    指示書カレンダー ルーティング・コントローラーの設定

--- app/controllers/toda1/silkscreen_a_controller.rb
+++ app/controllers/toda1/silkscreen_a_controller.rb
@@ -25,4 +25,24 @@ class Toda1::SilkscreenAController < ApplicationController
                 .where(orders: { internal_delivery_date: @date.to_date }).distinct
                 .order(:internal_delivery_date)
   end
+
+  def pasteup_wip
+    @date = params[:date]
+    @orders = Order.left_joins(order_details: :order_technique_details).distinct
+                .where(order_details: { factory_id: 4 }).distinct
+                .where(order_technique_details: { technique_id: 1 }).distinct
+                .where.not(order_technique_details: { progress_id: [4, 5, 6, 7] })
+                .where(orders: { internal_delivery_date: @date.to_date }).distinct
+                .order(:internal_delivery_date)
+  end
+
+  def pasteup_done
+    @date = params[:date]
+    @orders = Order.left_joins(order_details: :order_technique_details).distinct
+                .where(order_details: { factory_id: 4 }).distinct
+                .where(order_technique_details: { technique_id: 1 }).distinct
+                .where(order_technique_details: { progress_id: [4, 5, 6, 7] })
+                .where(orders: { internal_delivery_date: @date.to_date }).distinct
+                .order(:internal_delivery_date)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,6 @@ Rails.application.routes.draw do
 
   namespace :pasteup do
     resources :calendar, only: :index
-
   end
 
   namespace :domestic_buying do
@@ -47,6 +46,8 @@ Rails.application.routes.draw do
     resources :blank, only: :index do
       get :wip, on: :collection
       get :done, on: :collection
+      get :pasteup_wip, on: :collection
+      get :pasteup_done, on: :collection
     end
     resources :all, only: :index
     resources :sorting, only: :index do
@@ -56,19 +57,27 @@ Rails.application.routes.draw do
     resources :silkscreen_a, only: :index do
       get :wip, on: :collection
       get :done, on: :collection
+      get :pasteup_wip, on: :collection
+      get :pasteup_done, on: :collection
     end
     resources :inkjet, only: :index do
       get :wip, on: :collection
       get :done, on: :collection
+      get :pasteup_wip, on: :collection
+      get :pasteup_done, on: :collection
     end
     resources :embroidery, only: :index do
       get :wip, on: :collection
       get :done, on: :collection
+      get :pasteup_wip, on: :collection
+      get :pasteup_done, on: :collection
       get :report, on: :collection
     end
     resources :sewing, only: :index do
       get :wip, on: :collection
       get :done, on: :collection
+      get :pasteup_wip, on: :collection
+      get :pasteup_done, on: :collection
     end
   end
 
@@ -76,6 +85,8 @@ Rails.application.routes.draw do
     resources :blank, only: :index do
       get :wip, on: :collection
       get :done, on: :collection
+      get :pasteup_wip, on: :collection
+      get :pasteup_done, on: :collection
     end
     resources :all, only: :index
     resources :sorting, only: :index do
@@ -85,30 +96,44 @@ Rails.application.routes.draw do
     resources :silkscreen_a, only: :index do
       get :wip, on: :collection
       get :done, on: :collection
+      get :pasteup_wip, on: :collection
+      get :pasteup_done, on: :collection
     end
     resources :inkjet, only: :index do
       get :wip, on: :collection
       get :done, on: :collection
+      get :pasteup_wip, on: :collection
+      get :pasteup_done, on: :collection
     end
     resources :embroidery, only: :index do
       get :wip, on: :collection
       get :done, on: :collection
+      get :pasteup_wip, on: :collection
+      get :pasteup_done, on: :collection
     end
     resources :sewing, only: :index do
       get :wip, on: :collection
       get :done, on: :collection
+      get :pasteup_wip, on: :collection
+      get :pasteup_done, on: :collection
     end
     resources :silkscreen_b, only: :index do
       get :wip, on: :collection
       get :done, on: :collection
+      get :pasteup_wip, on: :collection
+      get :pasteup_done, on: :collection
     end
     resources :silkscreen_c, only: :index do
       get :wip, on: :collection
       get :done, on: :collection
+      get :pasteup_wip, on: :collection
+      get :pasteup_done, on: :collection
     end
     resources :uv, only: :index do
       get :wip, on: :collection
       get :done, on: :collection
+      get :pasteup_wip, on: :collection
+      get :pasteup_done, on: :collection
     end
   end
 
@@ -116,10 +141,14 @@ Rails.application.routes.draw do
     resources :blank, only: :index do
       get :wip, on: :collection
       get :done, on: :collection
+      get :pasteup_wip, on: :collection
+      get :pasteup_done, on: :collection
     end
     resources :silkscreen_d, only: :index do
       get :wip, on: :collection
       get :done, on: :collection
+      get :pasteup_wip, on: :collection
+      get :pasteup_done, on: :collection
     end
   end
 
