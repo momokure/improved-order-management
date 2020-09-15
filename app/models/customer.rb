e9832bea1e2b02c91bed7548bff96cac5002ba77
hanaharafumiki <hanahara@cornerprinting.com>
2020-09-15 13:41:40 +0900

    customers関連の修正

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -10,21 +10,21 @@ class Customer < ApplicationRecord
 
   has_many :company_customers
   has_many :companies, through: :company_customers
-  accepts_nested_attributes_for :companies, reject_if: :all_blank
+  accepts_nested_attributes_for :companies, reject_if: :all_blank, allow_destroy: true
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture_code
-  has_many :customer_addresses
-  accepts_nested_attributes_for :customer_addresses
+  has_many :customer_addresses, dependent: :destroy, inverse_of: :customer
+  accepts_nested_attributes_for :customer_addresses, allow_destroy: true
 
-  has_many :customer_notes
-  accepts_nested_attributes_for :customer_notes, reject_if: :all_blank
+  has_many :customer_notes, dependent: :destroy, inverse_of: :customer
+  accepts_nested_attributes_for :customer_notes, reject_if: :all_blank, allow_destroy: true
 
-  has_many :customer_emails
-  accepts_nested_attributes_for :customer_emails, reject_if: :all_blank
+  has_many :customer_emails, dependent: :destroy, inverse_of: :customer
+  accepts_nested_attributes_for :customer_emails, reject_if: :all_blank, allow_destroy: true
 
-  has_many :customer_phone_numbers
-  accepts_nested_attributes_for :customer_phone_numbers, reject_if: :all_blank
+  has_many :customer_phone_numbers, dependent: :destroy, inverse_of: :customer
+  accepts_nested_attributes_for :customer_phone_numbers, reject_if: :all_blank, allow_destroy: true
 
   has_many :orders
 end
