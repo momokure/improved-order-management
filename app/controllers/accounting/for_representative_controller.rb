3c6a3b76f671cd652005f30d48f302ce3310d574
hanaharafumiki <hanahara@cornerprinting.com>
2020-09-25 17:26:13 +0900

    Update for_representative_controller.rb

--- app/controllers/accounting/for_representative_controller.rb
+++ app/controllers/accounting/for_representative_controller.rb
@@ -1,35 +1,39 @@
 class Accounting::ForRepresentativeController < ApplicationController
   require "date"
+
   def index
     @orders = Order.left_joins(:payments).distinct
                 .where(orders: { order_type_id: 1 }).distinct
-                .order(order_date: "DESC")
+                .where(payments: { order_id: nil }).distinct
+                .order(Arel.sql('internal_delivery_date IS NULL, internal_delivery_date ASC'))
+                .order(Arel.sql('payment_deadline_date IS NULL, payment_deadline_date ASC'))
+
   end
 
-  # def index
-  #   @orders = Order.left_joins(:buy_details, :order_details).distinct
-  #               .where(orders: { domestic_buying: 1 }).distinct #国内発注あり
-  #               .where.not(orders: { internal_delivery_date: nil }).distinct #納品日空欄
-  #               .where(buy_details: { order_id: nil }).distinct #発注部署記載なし
-  #               .order(Arel.sql('internal_delivery_date IS NULL, internal_delivery_date ASC'))
-  #               .order(Arel.sql('order_details.factory_id'))
-  #               .order(:order_type_id)
-  #               .or(
-  #                 Order.left_joins(:buy_details, :order_details).distinct
-  #                   .where(orders: { domestic_buying: 1 }).distinct #国内発注あり
-  #                   .where(buy_details: { buy_type_id: 1 }).distinct #国内発注部署記載あり
-  #                   .where.not(buy_details: { buy_progress_id: 2 }).distinct #国内発注部署記載あり(発注完了以外)
-  #                   .order(Arel.sql('internal_delivery_date IS NULL, internal_delivery_date ASC'))
-  #                   .order(Arel.sql('order_details.factory_id'))
-  #                   .order(:order_type_id)
-  #               )
-  #               .or(
-  #                 Order.left_joins(:buy_details, :order_details).distinct
-  #                   .where(orders: { domestic_buying: 1 }).distinct #国内発注あり
-  #                   .where(orders: { overseas_buying: 1 }).distinct #海外発注あり
-  #                   .order(Arel.sql('internal_delivery_date IS NULL, internal_delivery_date ASC'))
-  #                   .order(Arel.sql('order_details.factory_id'))
-  #                   .order(:order_type_id)
-  #               )
-  # end
+  #ご入金予定額 - (すべてのご入金額) = 過不足額
+  # 　0 == ご入金済、0 > 不足、1=< 超過
+
+
+  # difference(差額)カラムを作成
+  # カラムには下記が 自動で更新されるようにする
+  # ご入金額(amount_paid) - 入金予定額(payment_amount)
+
+
+  ## フィルタ
+  # ①customer_typeが 前払い(請求書不要)
+  # ②ご入金 (Payment) がない
+  # 　→基本的に不備・後納は除外する
+  # 　→ただし、不備・後納でもご入金が発生する場合は 含まれる
+
+  # ③ご入金 (Payment) があっても 不足金がある
+
+  # ④ご入金 (Payment) があっても 超過金がある
+
+  ## 並び順
+  # 不備・後納は基本的に除外する (例外あり)
+  # 基本は納品日順 (過不足→明日出荷予定の順で表示する)
+  # *請求書払いのお客様は表示しない(別シートで管理する)
+
+  # 請求書払いを登録する
+  # あと何日！を表示
 end
