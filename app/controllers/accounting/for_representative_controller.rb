b9f539a920f2ce8a92a0c062426420279e16b5f3
hanaharafumiki <hanahara@cornerprinting.com>
2020-09-29 22:34:34 +0900

    未入金アナウンスのビューを修正

--- app/controllers/accounting/for_representative_controller.rb
+++ app/controllers/accounting/for_representative_controller.rb
@@ -2,10 +2,18 @@ class Accounting::ForRepresentativeController < ApplicationController
   require "date"
 
   def index
-    @orders = Order.left_joins(:payments).distinct
-                .where.not(orders: { difference: 0 }).distinct
+    excess = [1..Float::INFINITY]
+    deficiency = [-Float::INFINITY..-1]
+
+    @excess_orders = Order.where(difference: excess).distinct
+                .where(payment_method_id: [1, 2])
+                .order(Arel.sql('difference DESC'))
                 .order(Arel.sql('internal_delivery_date IS NULL, internal_delivery_date ASC'))
-                .order(Arel.sql('difference ASC'))
                 .order(Arel.sql('payment_deadline_date IS NULL, payment_deadline_date ASC'))
+
+    @deficiency_orders = Order.where(difference: deficiency).distinct
+                           .where(payment_method_id: [1, 2])
+                           .order(Arel.sql('internal_delivery_date IS NULL, internal_delivery_date ASC'))
+                           .order(Arel.sql('payment_deadline_date IS NULL, payment_deadline_date ASC'))
   end
 end
--- lib/tasks/calculate.rake
+++ lib/tasks/calculate.rake
@@ -3,14 +3,15 @@ namespace :calculate do
   task difference: :environment do
     Order.all.each do |order|
       amount_paid = Payment.where(order_id: order.id).sum(:amount_paid)
-      if Payment.where(order_id: order.id).present? && order.payment_amount.present?
+
+      if Payment.where(order_id: order.id).blank? && order.payment_amount.present?
+        order.difference = order.payment_amount.to_i * -1
+        order.save
+      elsif Payment.where(order_id: order.id).present? && order.payment_amount.present?
         order.difference = amount_paid - order.payment_amount
         order.save
       elsif Payment.where(order_id: order.id).present? && order.payment_amount.nil?
         order.difference = amount_paid
-        order.save
-      elsif Payment.where(order_id: order.id).nil? && order.payment_amount.present?
-        order.difference = order.payment_amount * -1
         order.save
       end
     end
