65e1ceed9a044861992752b2591c957f00331cf5
cpnao <cpnao@users.noreply.github.com>
2020-10-01 20:01:52 +0900

    fontファイルの追加

--- app/controllers/pasteup_controller.rb
+++ app/controllers/pasteup_controller.rb
@@ -1,23 +1,44 @@
 class PasteupController < ApplicationController
   require "date"
   before_action :set_params
+  @@d = Date.today
+  $thisyear
 
   def toda1_silkscreen_a
     @order
   end
 
   def toda1_inkjet
-  end
-
-  def toda1_embroidery
     @order = Order.find_by(uid: params[:id])
     @customer = Customer.find(@order.customer_id)
     respond_to do |format|
       format.html
       format.pdf do
-      render pdf: @order.uid+' '+@customer.customer_name+'戸田1 emb'+Date.today.year,   # PDF名
+      render pdf: @order.uid+' '+@customer.customer_name+' 戸田1 ink'+@@d.year.to_s,   # PDF名
         encording: 'UTF-8',
-        template: 'pasteup/toda1_embroidery.html.haml' # viewを対象にする
+        template: 'pasteup/toda1_inkjet.html.haml' # viewを対象にする
+      end
+    end
+  end
+
+  def toda1_embroidery
+    d = Date.today
+    @order = Order.find_by(uid: params[:id])
+    @customer = Customer.find(@order.customer_id)
+    respond_to do |format|
+      format.html
+      format.pdf do
+      render pdf: @order.uid+' '+@customer.customer_name+' 戸田1 emb'+@@d.year.to_s,   # PDF名
+        encording: 'UTF-8',
+        template: 'pasteup/toda1_embroidery.html.haml', # viewを対象にする
+        page_size: 'A4',
+               margin: { top: 2,
+                         bottom: 0,
+                         left: 0,
+                         right: 0 },
+                        #outline: {   outline:           true,
+                        #    outline_depth:     LEVEL },
+        show_as_html: params[:debug].present? # debug するか？
       end
     end
   end
