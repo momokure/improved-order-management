178a80ab1b1efa992024abf65987091840ef907d
hanaharafumiki <hanahara@cornerprinting.com>
2020-10-02 18:50:01 +0900

    ツールチップの実装

--- app/assets/javascripts/text_overflow.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-  $('.overflow-tooltip').bind('mouseover', function(){
-    var $this = $(this);
-    if (this.offsetWidth < this.scrollWidth && !$this.attr('title')){
-      $this.attr('title', $this.text());
-    }
-  });
-});
